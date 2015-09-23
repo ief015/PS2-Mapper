0e3f12abf179a2902a262e1e6178034cac2cb9e2
Nathan Cousins <ief015@gmail.com>
2015-09-23 14:19:17 -0400

    Update pumpkin maps
    +12 indar
    
    also welcome back.

---
+++ compile-snowmen.bat
@@ -0,0 +1,4 @@
+@"bin/luajit.exe" lua/compile.lua snowmen/amerish.txt Amerish.jpg out/snowmen-amerish.jpg
+@"bin/luajit.exe" lua/compile.lua snowmen/esamir.txt Esamir.jpg out/snowmen-esamir.jpg
+@"bin/luajit.exe" lua/compile.lua snowmen/hossin.txt Hossin.jpg out/snowmen-hossin.jpg
+@"bin/luajit.exe" lua/compile.lua snowmen/indar.txt Indar.jpg out/snowmen-indar.jpg
